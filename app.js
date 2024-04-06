1fe7e4edd02703d200f601f810e6abdda0438ce9
Zhu-Peter <gzhu@ualberta.ca>
2024-04-05 19:30:31 -0600

    added most of required content

--- app.js
+++ app.js
@@ -4,5 +4,41 @@ let squareYellow = document.getElementById(`square_yellow`);
 let squareGreen = document.getElementById(`square_green`);
 let squarePurple = document.getElementById(`square_purple`);
 
+let originalColor = "";
+squareBlue.addEventListener("click", togglePink);
 
+function togglePink() {
+  // console.log('click');
+  if (!(this.style.backgroundColor === "pink")) {
+    originalColor = this.style.backgroundColor;
+    this.style.backgroundColor = "pink";
+    return;
+  } else if (this.style.backgroundColor === "pink") {
+    this.style.backgroundColor = originalColor;
+  }
+}
 
+squareRed.addEventListener("dblclick", togglePink);
+
+document.addEventListener("keydown", function (zEvent) {
+  if (zEvent.key === ' ') {
+    if (document.body.style.backgroundColor === 'black'){
+        document.body.style.backgroundColor = 'white';
+        squareYellow.style.borderRadius = "0%";
+        return
+    }
+    document.body.style.backgroundColor = 'black';
+    squareYellow.style.borderRadius = "50%";
+    zEvent.preventDefault();
+  }
+});
+
+let originalText = '';
+squarePurple.addEventListener("mouseover", function(){
+    originalText = this.innerHTML;
+    this.innerHTML = '<h1 id="yellow">You are Hovering over me!</h1>';
+    document.getElementById('yellow').style.color = 'yellow';
+})
+squarePurple.addEventListener("mouseleave", function(){
+    this.innerHTML = originalText;
+})
