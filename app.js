bbaa728d4c8ecbef91911b76485d83434216bf84
Zhu-Peter <gzhu@ualberta.ca>
2024-04-05 18:39:47 -0600

    added content

---
+++ app.js
@@ -0,0 +1,8 @@
+let squareBlue = document.getElementById(`square_blue`);
+let squareRed = document.getElementById(`square_red`);
+let squareYellow = document.getElementById(`square_yellow`);
+let squareGreen = document.getElementById(`square_green`);
+let squarePurple = document.getElementById(`square_purple`);
+
+
+
